f6b3eefc3923a99852e8915077b3961e4fe26107
Smit Patel <65167432+SmitPatel910@users.noreply.github.com>
2021-02-01 11:50:05 +0530

    Update TypewiseReport.php

--- TypewiseReport.php
+++ TypewiseReport.php
@@ -16,7 +16,10 @@ if (strlen($_SESSION['detsuid']==0)) {
 	<link href="css/font-awesome.min.css" rel="stylesheet">
 	<link href="css/datepicker3.css" rel="stylesheet">
 	<link href="css/styles.css" rel="stylesheet">
-	
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 	<!--Custom Font-->
 	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 </head>
@@ -54,7 +57,7 @@ if (strlen($_SESSION['detsuid']==0)) {
 									<label>To Date</label>
 									<input class="form-control" type="date"  id="todate" name="todate" required="true">
 								</div>
-                                <div class="form-group col-md-12">
+                                <div class="col-sm-">
                                 <label class="radio-inline">
 								<input  type="radio" name="optradio" value="General" required="required"/>General
 								</label>

fc2e1c18b59d9b870819c2e82ff1f22055231a6e
Smit Patel <65167432+SmitPatel910@users.noreply.github.com>
2021-02-03 22:32:32 +0530

    Update graph-detailed2.php

--- graph-detailed2.php
+++ graph-detailed2.php
@@ -125,6 +125,14 @@ else{
                         }
                     }
                 </style>    
+	<script src="js/jquery-1.11.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/chart.min.js"></script>
+	<script src="js/chart-data.js"></script>
+	<script src="js/easypiechart.js"></script>
+	<script src="js/easypiechart-data.js"></script>
+	<script src="js/bootstrap-datepicker.js"></script>
+	<script src="js/custom.js"></script>
 </body>
 </html>
 <?php } ?>

12734d6ac0bed2dc78a013c5020651d5d15afc54
Smit Patel <65167432+SmitPatel910@users.noreply.github.com>
2021-02-04 17:43:24 +0530

    Update sidebar.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -13,13 +13,12 @@ include('includes/dbconnection.php');
 
             <div class="profile-usertitle">
                 <?php
-$uid=$_SESSION['detsuid'];
-$ret=mysqli_query($con,"select FullName from tbluser where ID='$uid'");
-$row=mysqli_fetch_array($ret);
-$name=$row['FullName'];
-
-?>
-                <div class="profile-usertitle-name"><?php echo $name; ?></div>
+                $uid=$_SESSION['detsuid'];
+                $ret=mysqli_query($con,"select FullName from tbluser where ID='$uid'");
+                $row=mysqli_fetch_array($ret);
+                $name=$row['FullName'];
+                ?>
+                <div class="profile-usertitle-name"><a href="user-profile.php"><?php echo $name; ?></a></div>
                 <div class="profile-usertitle-status"><span class="indicator label-success"></span>Online</div>
             </div>
             <div class="clear"></div>
@@ -31,40 +30,64 @@ $name=$row['FullName'];
 
         <div class="divider"></div>
         <ul class="nav menu">
-            <li class="active"><a href="dashboard.php"><em class="fa fa-dashboard">&nbsp;</em> Dashboard</a></li>
+            <li class="active"><a href="dashboard.php"><em class="fa fa-dashboard">&nbsp;</em>Dashboard</a></li>
             
             <li class="parent "><a data-toggle="collapse" href="#sub-item-1">
-                <em class="fa fa-navicon">&nbsp;</em>Expenses <span data-toggle="collapse" href="#sub-item-1" class="icon pull-right"><em class="fa fa-plus"></em></span>
+                <em class="fa fa-navicon">&nbsp;</em>Income<span data-toggle="collapse" href="#sub-item-1" class="icon pull-right"><em class="fa fa-plus"></em></span>
                 </a>
+                
                 <ul class="children collapse" id="sub-item-1">
+                    <li><a class="" href="add-income.php">
+                        <span class="fa fa-arrow-right">&nbsp;</span> Add Income
+                    </a></li>
+                    <li><a class="" href="manage-income.php">
+                        <span class="fa fa-arrow-right">&nbsp;</span> Manage Income
+                    </a></li>
+                    <li><a class="" href="income-report.php">
+                        <span class="fa fa-arrow-right">&nbsp;</span> Income Report
+                    </a></li>
+                    <li><a class="" href="income-report-typewise.php">
+                        <span class="fa fa-arrow-right">&nbsp;</span> Typewise Income
+                    </a></li>
+                    
+                </ul>
+
+            </li>
+
+
+            <li class="parent "><a data-toggle="collapse" href="#sub-item-2">
+                <em class="fa fa-navicon">&nbsp;</em>Expenses <span data-toggle="collapse" href="#sub-item-2" class="icon pull-right"><em class="fa fa-plus"></em></span>
+                </a>
+                
+                <ul class="children collapse" id="sub-item-2">
                     <li><a class="" href="add-expense.php">
                         <span class="fa fa-arrow-right">&nbsp;</span> Add Expenses
                     </a></li>
                     <li><a class="" href="manage-expense.php">
                         <span class="fa fa-arrow-right">&nbsp;</span> Manage Expenses
                     </a></li>
-                    
+                    <li><a class="" href="expense-report.php">
+                        <span class="fa fa-arrow-right">&nbsp;</span> Expense Report 
+                    </a></li>
+                    <li><a class="" href="Expense-report-typewise.php">
+                        <span class="fa fa-arrow-right">&nbsp;</span> Typewise Expense
+                    </a></li>
                 </ul>
 
             </li>
            
-            <li class="parent "><a data-toggle="collapse" href="#sub-item-2">
-                <em class="fa fa-navicon">&nbsp;</em> Expense Report <span data-toggle="collapse" href="#sub-item-1" class="icon pull-right"><em class="fa fa-plus"></em></span>
+            <li class="parent "><a data-toggle="collapse" href="#sub-item-3">
+                <em class="fa fa-navicon">&nbsp;</em>Generate Report<span data-toggle="collapse" href="#sub-item-3" class="icon pull-right"><em class="fa fa-plus"></em></span>
                 </a>
-                <ul class="children collapse" id="sub-item-2">
-                    <li><a class="" href="expense-datewise-reports.php">
-                        <span class="fa fa-arrow-right">&nbsp;</span>Generate Report 
-                    </a></li>
-                    <li><a class="" href="TypewiseReport.php">
-                        <span class="fa fa-arrow-right">&nbsp;</span> Type Wise Report
-                    </a></li>
+                <ul class="children collapse" id="sub-item-3">
                     <li><a class="" href="graph.php">
-                        <span class="fa fa-arrow-right">&nbsp;</span> Generate Graph
+                        <span class="fa fa-arrow-right">&nbsp;</span>Net Saving
                     </a></li>
                 </ul>
             </li>
 
-            <li><a href="user-profile.php"><em class="fa fa-user">&nbsp;</em> Profile</a></li>
+
+            <!-- <li><a href="user-profile.php"><em class="fa fa-user">&nbsp;</em> Profile</a></li> -->
              
             <li><a href="change-password.php"><em class="fa fa-clone">&nbsp;</em> Change Password</a></li>
 

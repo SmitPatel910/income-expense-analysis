0edf57f8c2e5fc75a239e24b3bc742d13bb737b6
Smit Patel <65167432+SmitPatel910@users.noreply.github.com>
2022-06-11 16:35:28 +0530

    Update dbconnection.php

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -3,7 +3,8 @@
 // $con=mysqli_connect("localhost", "root", "", "detsdb");
 
 // Remote Database Connecion
-$con=mysqli_connect("remotemysql.com", "q5xfeB8J8l", "yrM6dmI72x", "q5xfeB8J8l");
+$con=mysqli_connect("localhost", "id19087684_incomeexpenseuser", "4BJfF1nT>hUt*kJd", "id19087684_incomeexpense");
+// $con=mysqli_connect("remotemysql.com", "q5xfeB8J8l", "yrM6dmI72x", "q5xfeB8J8l");
 
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
